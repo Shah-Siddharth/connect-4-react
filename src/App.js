fb7b484737a1b0dca97eb101e7449ff259537e72
Siddharth <siddharthshah1102@gmail.com>
2023-02-20 18:47:02 +0530

    correctly update gameBoard state, rendering GameCircles as list using function

--- src/App.js
+++ src/App.js
@@ -1,73 +1,45 @@
 import { useState } from "react";
 import GameCircle from "./components/GameCircle";
 
-const NO_PLAYER = 0
-const PLAYER_1 = 1
-const PLAYER_2 = 2
+const NO_PLAYER = 0;
+const PLAYER_1 = 1;
+const PLAYER_2 = 2;
+const NO_OF_CIRCLES = 16;
 
 function App() {
 
   const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
 
+  const renderGameCircles = () => {
+    const circles = [];
+    for (let i=0; i<NO_OF_CIRCLES; i++) {
+      circles.push(
+      <GameCircle
+        id={i}
+        key={i}
+        className={`player-${gameBoard[i]}`}
+        onCircleClicked={gameCircleClicked}
+      />
+      )
+    }
+
+    return circles;
+  }
+
   const gameCircleClicked = (id) => {
-    
-    gameBoard[id] = currentPlayer;
-    
-    setGameBoard(gameBoard);
+    let newGameBoard = gameBoard.map((elem, idx) => {
+      if (idx === id) return currentPlayer;
+      return elem;
+    });
+
+    setGameBoard(newGameBoard);
     setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
   }
 
   return (
     <div className="gameBoard">
-      <GameCircle id={1} onCircleClicked={gameCircleClicked}>
-      </GameCircle>
-
-      <GameCircle id={2}>
-      </GameCircle>
-
-      <GameCircle id={3}>
-      </GameCircle>
-
-      <GameCircle id={4}>
-      </GameCircle>
-
-      <GameCircle id={5}>
-      </GameCircle>
-
-      <GameCircle id={6}>
-      </GameCircle>
-
-      <GameCircle id={7}>
-      </GameCircle>
-
-      <GameCircle id={8}>
-      </GameCircle>
-
-      <GameCircle id={9}>
-      </GameCircle>
-
-      <GameCircle id={10}>
-      </GameCircle>
-
-      <GameCircle id={11}>
-      </GameCircle>
-
-      <GameCircle id={12}>
-      </GameCircle>
-
-      <GameCircle id={13}>
-      </GameCircle>
-
-      <GameCircle id={14}>
-      </GameCircle>
-
-      <GameCircle id={15}>
-      </GameCircle>
-
-      <GameCircle id={16}>
-      </GameCircle>
-
+      {renderGameCircles()}
     </div>
   );
 }
